e6e2d24cc85f432b506f32cda15d384b4b8f2aea
Tseringdhoundup9813 <tseringlama9813694977@gmail.com>
2023-08-06 14:43:03 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express()
+
+const port = process.env.PORT||3000
+
+app.get('/',(req,res)=>{
+    res.send("<h1>hello tsering you are doing great</h1>")
+})
+app.get("/api",(req,res)=>{
+    res.send("learning how to deploy node js to aws ")
+})
+app.listen(port,()=>{
+    console.log(`server is listening on ${port}`)
+})
+
